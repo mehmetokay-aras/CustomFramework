8a389c66f3c10869a7ec78d8f094e9d6ecc127b9
okaymehmetali <mehmetaliokay19@gmail.com>
2023-09-19 15:07:02 +0300

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,34 @@
+//
+//  SDK.swift
+//  package-demo-ios
+//
+//  Created by Mehmet Ali Okay on 19.09.2023.
+//
+
+import Foundation
+
+public struct SDK {
+    
+//    private static var isAPIKeyValid = false
+//
+//    private static let apiKeys = [
+//        "privateKey123"
+//    ]
+//
+//    private static func checkAPIKey() -> Bool {
+//        if !isAPIKeyValid {
+//            print("Please provide a valid API Key")
+//        }
+//        return isAPIKeyValid
+//    }
+//
+//    public static func configure(apiKey: String) {
+//        isAPIKeyValid = apiKeys.contains(apiKey)
+//    }
+    
+    public static func doSomeWork() {
+//        guard checkAPIKey() else { return }
+        print("Doing some work...")
+    }
+    
+}
